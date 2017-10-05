5eed510a78578093a3045771099ea0a4dc3f85a9
Meriton <meriton.ferati@hotmail.com>
2017-10-05 14:33:45 +0200

    Add files via upload

---
+++ Programming tutorials Project (not finished)/scripts/javascript.js
@@ -0,0 +1,18 @@
+function valid(){
+							var passid = document.registration.passid;
+							var uname = document.registration.username;
+							var uemail = document.registration.email;
+							if { 
+								if(validatePassword(passid,7,12))
+								{
+									if(allLetter(uname))
+									{
+										if(ValidateEmail(uemail))
+										{
+											
+										}
+									}
+								}
+							}
+							return alert("Hello! I am an alert box!");;
+				}
